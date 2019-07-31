b5a228e74afedf003f96d6aae4d897b4afddd1eb
Daniel Husar <dano.husar@gmail.com>
2019-07-30 22:06:49 -0700

    Fix header

--- demo/src/@daniel.husar/gatsby-theme-spring/components/header.tsx
+++ demo/src/@daniel.husar/gatsby-theme-spring/components/header.tsx
@@ -34,11 +34,11 @@ export default function Header() {
       <H1>Hi there!</H1>
       <p>My name is Daniel, and I’m a product engineer based in San Francisco.</p>
       <p>Right now I’m working on Intercom Messenger to make internet business personal. Check out my blog or follow me on social media.</p>
-      <p>
+      <div>
         <SocialIcon url="https://twitter.com/danohusar" style={{ height: 35, width: 35 }} />
         <SocialIcon url="https://github.com/danielhusar" style={{ height: 35, width: 35 }} />
         <SocialIcon url="https://www.instagram.com/efrafa/" style={{ height: 35, width: 35 }} />
-      </p>
+      </div>
     </MyHeaderStyled>
   );
 }
