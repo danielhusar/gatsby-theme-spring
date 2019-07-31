e12c40cab1f9b62712611b7eba695e5065f22259
Daniel Husar <dano.husar@gmail.com>
2019-07-31 16:01:01 -0700

    Add dateTime property to meta

--- theme/src/components/meta.tsx
+++ theme/src/components/meta.tsx
@@ -3,13 +3,18 @@ import { Meta as MetaStyled } from '@styles/meta';
 
 interface Props {
   timeToRead: number;
-  date: Date | null;
+  date: Date;
 }
 
 export default function Meta({ date, timeToRead }: Props) {
+  const formatedDate = new Date(date).toLocaleDateString('eu', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
   return (
     <MetaStyled>
-      <time>{date}</time>
+      <time dateTime={formatedDate}>{date}</time>
       <span>•</span>
       {timeToRead} minute{timeToRead > 1 ? 's' : ''} read
     </MetaStyled>
